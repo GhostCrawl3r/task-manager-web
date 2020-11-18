e3dc63c14c90b427abcf7bc9fe03066108265e73
Nick Morgan <nick@social-sync.com>
2020-11-18 23:37:02 +0000

    Started to make this application. I've setup the Global State Management system... Will need to make a more advanced version for this app. I'm just setting up the Drawer now.

---
+++ src/Context/GlobalContext.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GlobalContext = ({children}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default GlobalContext;
